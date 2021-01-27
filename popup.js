e43de6695b67bec1887bf1a3ae99cafe6db4a54e
Дарья Соколова <sklv.d01@gmail.com>
2021-01-27 15:02:01 +0400

    переходы по ссылкам

--- popup.js
+++ popup.js
@@ -4,26 +4,28 @@ const decorateTime = (seconds) => {
     let minutes = parseInt(seconds / 60, 10);
     let hours = parseInt(minutes / 60, 10);
 
+    hours = Math.floor(seconds / 60 / 60);;
+    minutes = Math.floor(seconds / 60) - (hours * 60);;
+    seconds = seconds % 60;
+    
     minutes = minutes < 10 ? `0${minutes}` : minutes;
     hours = hours < 10 ? `0${hours}` : hours;
     seconds = seconds < 10 ? `0${seconds}` : seconds;
 
     return `:${hours}:${minutes}:${seconds}`
-
 }
 
-const siteItem = (hostname, seconds) => `<li>
-<a href="" target="_blank">
+const siteItem = (hostname, seconds, href) => `<li>
+<a href="${href}" target="_blank">
   ${hostname} : ${decorateTime(seconds)}
 </a>
 </li>`
 
 const backgroundWindow = chrome.extension.getBackgroundPage();
-console.log(backgroundWindow);
 const urlContainer = backgroundWindow.urlContainer || { 'test': 4 };
 
 
 for (let key in urlContainer) {
-    list.innerHTML += siteItem(key, urlContainer[key]);
+    list.innerHTML += siteItem(key, urlContainer[key].seconds, urlContainer[key].href);
 }
 
--- util.js
+++ util.js
@@ -1,47 +1,66 @@
-window.urlContainer = 
-{
-    //"javascript.ru" : 0,
-    //"learn.javascript.ru" : 0,
-    //"ipsilon.sgu.ru": 0,
-    //"vk.com" : 0,
-    //"other" : 0,
-};
+const IS_DEBUG = false;
+window.urlContainer = {};
+
+// {
+//     'pornhub': {
+//         href,
+//             seconds,
+//     }
+// }
+
+
+// {
+//     //"javascript.ru" : 0,
+//     //"learn.javascript.ru" : 0,
+//     //"ipsilon.sgu.ru": 0,
+//     //"other" : 0,
+// };
 
 const getHotname = () => {
     return new Promise((resolve, reject) => {
         chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
             const url = tabs[0] && tabs[0].url;
-            if (url === undefined)
-            {
-                const hostname = 'console';
-                resolve(hostname);
+
+            try {
+                const { hostname, href } = new URL(url);
+                if ((hostname === 'extensions')||(hostname === 'bodjmonnkcegkkiofjpbliihjpkipdif')||(hostname === 'newtab')) {
+                    resolve({ hostname: 'режим разработчика', href: 'https://www.google.ru/', });
+                }
+                else {
+                    resolve({ hostname, href });
+                }
+
+            } catch (error) {
+                resolve({
+                    hostname: 'режим разработчика',
+                    href: 'https://www.google.ru/',
+                });
             }
-            else
-            {
-                const { hostname } = new URL(url);
-                resolve(hostname);
-            }
+
         });
     })
 }
 
 const intervalId = setInterval(() => {
-    getHotname().then(hostname => {
-        //console.log(hostname);
-        let mess = String(hostname);
-        if (mess in urlContainer)
-        {
-            urlContainer[mess] += 1;
+    getHotname().then(siteObj => {
+        const { hostname } = siteObj;
+
+        if (hostname in urlContainer) {
+            // urlContainer[mess] += 1;
+            urlContainer[hostname] = {
+                ...urlContainer[hostname],
+                seconds: urlContainer[hostname].seconds + 1,
+            };
         }
-        else
-        {
-            urlContainer[hostname] = 0;
+        else {
+            urlContainer[hostname] = {
+                href: siteObj.href,
+                seconds: 0,
+            };
         }
 
-        for (let key in urlContainer) 
-        {
-            console.log( key, urlContainer[key] );
-            //return `${key} ${myUrls[key]}`;
+        for (let key in urlContainer) {
+            IS_DEBUG && console.log(key, urlContainer[key]);
         }
     })
 }, 1000);
